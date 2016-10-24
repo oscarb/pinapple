eb606ef668842d8711a7505387ddbbf92a7b7a51
Oscar Björkman <git@oscarb.se>
2016-10-24 03:49:10 +0200

    Improved UI (#31)
    * Improve passcode UI
    * Refactor, translate and improve overall code and UI

--- app/src/main/java/se/oscarb/pinapple/PasscodeActivity.java
+++ app/src/main/java/se/oscarb/pinapple/PasscodeActivity.java
@@ -2,7 +2,7 @@ package se.oscarb.pinapple;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -16,7 +16,6 @@ public class PasscodeActivity extends AppCompatActivity {
     public final static String PASSCODE_MESSAGE = "se.oscarb.pinapple.PASSCODE";
     private static final String TAG = PasscodeActivity.class.getSimpleName();
     private EditText passcodeEditText;
-    private TextInputLayout textInputLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +26,6 @@ public class PasscodeActivity extends AppCompatActivity {
 
         // Initialize fields
         passcodeEditText = (EditText) findViewById(R.id.passcode);
-        textInputLayout = (TextInputLayout) findViewById(R.id.input_layout);
-
-
         passcodeEditText.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -56,7 +52,6 @@ public class PasscodeActivity extends AppCompatActivity {
     // Start MainActivity
     public void showCodes(View view) {
         showCodes();
-
     }
 
     private void showCodes() {
@@ -66,16 +61,22 @@ public class PasscodeActivity extends AppCompatActivity {
             int passcode = Integer.parseInt(passcodeEditText.getText().toString());
 
             Intent toMainActivityIntent = new Intent(this, MainActivity.class);
-            //toMainActivityIntent.setFlags(toMainActivityIntent.getFlags() | Intent.FLAG_ACTIVITY_NO_HISTORY);
             toMainActivityIntent.putExtra(PASSCODE_MESSAGE, passcode);
             startActivity(toMainActivityIntent);
             passcodeEditText.setText("");
-            textInputLayout.setError(null);
-            //finish();
 
         } catch (NumberFormatException exception) {
             Log.w(TAG, "NumberFormatException" + exception.getMessage());
         }
 
     }
+
+    public void showHelp(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //builder.setTitle("");
+        builder.setMessage(R.string.passcode_information);
+        builder.setPositiveButton(R.string.got_it, null);
+        builder.create().show();
+    }
+
 }
